24c910fc9640303c9376c4bf2bcbf05d004d4696
xTCry [Vladislav Kh] <wolt6@mail.ru>
2024-02-04 01:21:52 +0300

    chore(controller): update decorators

--- src/models/auth/auth.controller.ts
+++ src/models/auth/auth.controller.ts
@@ -6,7 +6,6 @@ import {
   Post,
   UseGuards,
   UseInterceptors,
-  Version,
   BadRequestException,
 } from '@nestjs/common';
 import { ReqAuth } from '@my-common';
@@ -15,24 +14,21 @@ import { LocalAuthGuard } from './guards';
 import { AuthService } from './auth.service';
 import { RequestAuthRestoreDto } from './dto/request-auth-restore.dto';
 
-@Controller('auth')
+@Controller({ path: 'auth', version: '1' })
+@UseInterceptors(ClassSerializerInterceptor)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Version('1')
   @Post('signin')
   @UseGuards(/* CaptchaGuard, */ LocalAuthGuard)
   @HttpCode(200)
-  @UseInterceptors(ClassSerializerInterceptor)
   async postSignin(@ReqAuth() user) {
     const response = await this.authService.authUser(user);
     return response;
   }
 
-  @Version('1')
   @Post('restore')
   @HttpCode(200)
-  @UseInterceptors(ClassSerializerInterceptor)
   async postRestore(@Body() body: RequestAuthRestoreDto) {
     const response = await this.authService.restoreAuth(
       body.cardNumber,
