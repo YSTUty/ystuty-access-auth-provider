478fd27e2880bf372097bd8871468a5a13bb89ba
xTCry [Vladislav Kh] <wolt6@mail.ru>
2024-05-04 20:34:07 +0300

    chore(common): update `HttpExceptionFilter`

--- src/common/filter/http-exception.filter.ts
+++ src/common/filter/http-exception.filter.ts
@@ -8,15 +8,15 @@ import {
 import * as rxjs from 'rxjs';
 import { Response } from 'express';
 
+/**
+ * Фильр ошибок для овтета на HTTP или RPC
+ */
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     if (host.getType() !== 'http' && host.getType() !== 'rpc') {
       return exception;
     }
-
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
 
     const code = exception.getStatus() || HttpStatus.INTERNAL_SERVER_ERROR;
     const expResponse = exception.getResponse() as string | any;
@@ -42,9 +42,9 @@ export class HttpExceptionFilter implements ExceptionFilter {
     };
 
     if (host.getType() === 'rpc') {
-      return rxjs.throwError(() => error);
+      return rxjs.throwError(() => ({ status: 'error', ...error }));
     }
 
-    response.status(code).json({ error });
+    host.switchToHttp().getResponse<Response>().status(code).json({ error });
   }
 }
